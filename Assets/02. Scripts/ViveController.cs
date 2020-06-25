7ee36de7f37acdde32b0cd686473017630d5ab88
hippoapple <39970069+hippoapple@users.noreply.github.com>
2020-06-25 14:54:31 +0900

    스크립트 수정

---
+++ Assets/02. Scripts/ViveController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class ViveController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 컨트롤러 정의
+    public SteamVR_Input_Sources leftHand;
+    public SteamVR_Input_Sources rightHand;
+    public SteamVR_Input_Sources any;
+    
+    // 컨트ㅗㄹ러 입력값 정의
+    public SteamVR_Action_Boolean trigger;
+
+    void Awake()
+    {
+        trigger = SteamVR_Actions.default_InteractUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SteamVR_Actions.default_InteractUI.GetStateDown(leftHand))
+        {
+            Debug.Log("Clicked Trigger Button");
+        }
+
+    }
+}
